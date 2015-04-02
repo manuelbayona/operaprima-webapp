11d5c38146b2dda5ff7d2edf2085a98652d2251e
Javier Lacalle <javihojo@gmail.com>
2015-04-02 12:56:50 +0200

    [FIX] login

--- webapp/src/main/webapp/js/components/directives/login/login.js
+++ webapp/src/main/webapp/js/components/directives/login/login.js
@@ -22,7 +22,7 @@ app.directive('login', function(){
 		},
 		require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
 		restrict: 'EA', // E = Element, A = Attribute, C = Class, M = Comment
-		templateUrl: 'js/components/directives/login/template/loginTemplate.html',
+		templateUrl: 'js/components/directives/login/templates/loginTemplate.html',
 		// replace: true,
 		// transclude: true,
 		//link: function($scope, iElm, iAttrs, controller) {
