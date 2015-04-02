d85847d15eba381a3300aad1e4cdbfb9266efbcc
Javier Lacalle <javihojo@gmail.com>
2015-04-02 09:20:13 +0200

    [ADD] primeras páginas

---
+++ webapp/src/main/webapp/js/app.js
@@ -0,0 +1,19 @@
+var app = angular.module('operaPrimaApp', ['ui.router']);
+
+app.config(function($stateProvider,$urlRouterProvider){
+    $stateProvider.state('operaPrima',{
+        url: '/operaPrima',
+        abstracts: true,
+        views: {
+            content: {
+                templateUrl:'js/commons/templates/content.html'
+            }
+        }
+    }).state('operaPrima.login',{
+        url: '/login',
+        templateUrl:'js/login/template/loginTemplate.html',
+        controller: 'loginCtrl'
+    });
+
+	$urlRouterProvider.otherwise('/operaPrima/login');
+});
