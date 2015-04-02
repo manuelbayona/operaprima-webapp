67e7d09aa9e4b2b68471b0fcbc3c263a1fce3cb2
Diego Asensio <diego.asensio.adesis@gmail.com>
2015-04-02 12:43:24 +0200

    [ADD] Layout

--- webapp/src/main/webapp/js/app.js
+++ webapp/src/main/webapp/js/app.js
@@ -1,19 +1,47 @@
 var app = angular.module('operaPrimaApp', ['ui.router']);
 
 app.config(function($stateProvider,$urlRouterProvider){
-    $stateProvider.state('operaPrima',{
-        url: '/operaPrima',
-        abstracts: true,
+    
+	$stateProvider
+	.state('login',{
+        abstract: true,
         views: {
-            content: {
+        	'layout': {
+                templateUrl:'js/login/template/loginLayout.html'
+            },
+            'content@login': {
                 templateUrl:'js/commons/templates/content.html'
             }
         }
-    }).state('operaPrima.login',{
+    })
+    .state('login.init',{
         url: '/login',
         templateUrl:'js/login/template/loginTemplate.html',
         controller: 'loginCtrl'
+    })
+    .state('dashboard',{
+    	url: '/dashboard',
+        abstract: true,
+        views: {
+        	'layout':{
+        		templateUrl:'js/dashboard/template/dashboardLayout.html',
+        	},
+        	'header@dashboard': {
+        		templateUrl: 'js/commons/templates/header.html'
+        	},
+            'content@dashboard': {
+                templateUrl:'js/commons/templates/content.html'
+            },
+            'footer@dashboard': {
+        		templateUrl: 'js/commons/templates/footer.html'
+        	}
+        }
+    })
+    .state('dashboard.init',{
+        url: '/init',
+        templateUrl:'js/dashboard/template/dashboardTemplate.html',
+        controller: 'DashboardCtrl'
     });
 
-	$urlRouterProvider.otherwise('/operaPrima/login');
+	$urlRouterProvider.otherwise('/login');
 });
