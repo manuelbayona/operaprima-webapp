60516d66586568ac262c0ca561bdc5078ebccdf5
Javier Lacalle <javihojo@gmail.com>
2015-04-09 12:40:51 +0200

    [ADD] Ajax

--- webapp/src/main/webapp/js/commons/factories/httpFactory.js
+++ webapp/src/main/webapp/js/commons/factories/httpFactory.js
@@ -1,10 +1,16 @@
-app.factory('httpFactory', ['$state', function($state){
+app.factory('httpFactory', ['$state','$http','$q', function($state,$http,$q){
 	
 	var ajaxCalls = {};
 
 	ajaxCalls.login = function(user) {
 		//TODO llamaremos al servidor para logear el usuario.
-		 $state.go('login.profiles');
+		// TODO usar promesas??
+		$state.go('login.profiles');
+	}
+
+	ajaxCalls.getProfiles = function(userId){
+		var urlLogin = 'http://localhost:8080/OperaPrima/services/V01/users/'+userId+'/persons';
+		return $http.get(urlLogin);
 	}
 
 	ajaxCalls.dashboard = function(){
--- webapp/src/main/webapp/js/profiles/controllers/profilesCtrl.js
+++ webapp/src/main/webapp/js/profiles/controllers/profilesCtrl.js
@@ -1,6 +1,18 @@
 app.controller('profilesCtrl', ['$rootScope','$scope','httpFactory', function($rootScope,$scope,httpFactory){
-	$scope.profiles = [{id:'1',name:'Javier',lastName: 'Lacalle',dni:'53538526J',birthDate:'13/10/1988',type:'STUDENT',state:'ACTIVE'},{id:'2',name:'Diego',lastName: 'Asensio',dni:'33538526J',birthDate:'13/10/1990',type:'STUDENT',state:'ACTIVE'},{id:'3',name:'Manuel',lastName: 'Bayona',dni:'99538526J',birthDate:'13/10/1982',type:'STUDENT',state:'ACTIVE'}];
+	// $scope.profiles = [{id:'1',name:'Javier',lastName: 'Lacalle',dni:'53538526J',birthDate:'13/10/1988',type:'STUDENT',state:'ACTIVE'},{id:'2',name:'Diego',lastName: 'Asensio',dni:'33538526J',birthDate:'13/10/1990',type:'STUDENT',state:'ACTIVE'},{id:'3',name:'Manuel',lastName: 'Bayona',dni:'99538526J',birthDate:'13/10/1982',type:'STUDENT',state:'ACTIVE'}];
+	
+	var successFn = function(response){
+		console.log(response.data)
+		$scope.profiles = response.data.persons;
+	}
 
+	var errorFn = function(error){
+		console.log(error);
+	}
+
+	var promise = httpFactory.getProfiles('1');
+	promise.then(successFn,errorFn);
+	
 	$scope.profileSelected = function(profile){
 		$rootScope.profileSelected = profile;
 		httpFactory.dashboard();
